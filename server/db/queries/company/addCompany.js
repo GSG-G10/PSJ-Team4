59ee31c87b2b2b486777ba182c607ecb8362906f
Eman Amassi <eman.amassi98@gmail.com>
2021-11-03 11:41:16 +0200

    Update addCompany.js

--- server/db/queries/company/addCompany.js
+++ server/db/queries/company/addCompany.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 module.exports = (name, email, password, location) => {
   const sqlQuery = {
-    text: 'INSERT INTO Company (name, email, password, location) Values ($1, $2,$3,$4) RETURNING id, profile_img;',
+    text: 'INSERT INTO Company (name, email, password, location) Values ($1, $2,$3,$4) RETURNING id',
     values: [name, email, password, location],
   };
   return connection.query(sqlQuery);
