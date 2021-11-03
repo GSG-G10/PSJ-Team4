cb91fe218af8c14ae0b1c95b1c7ee40b97bf7b95
Eman Amassi <eman.amassi98@gmail.com>
2021-11-03 11:31:00 +0200

    Update getEmployee.js

--- server/controllers/getEmployee.js
+++ server/controllers/getEmployee.js
@@ -2,12 +2,12 @@ const { getEmployeeById } = require('../db/queries/employee');
 
 const getEmployee = async (req, res, next) => {
   const { employeeId } = req.params;
-    try {
-      const { rows: data } = await getEmployeeById(employeeId);
-      res.json({ data });
-    } catch (err) {
-      next(err);
-    }
+  try {
+    const { rows: data } = await getEmployeeById(employeeId);
+    res.json({ data });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {getEmployee}
