c23ca7e6c1595a747d33ffcfb7fdc049b24af682
divluffy <div.jo2022@gmail.com>
2021-11-03 23:19:33 +0200

    fix coflict with raghd

---
+++ client/src/components/common/Nav/Login.jsx
@@ -0,0 +1,44 @@
+import {
+  Modal,
+} from 'antd';
+import './style.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import TypeAccount from './TypeAccount';
+import LoginForm from './LoginForm';
+
+function Login({ setIsModalVisible, isModalVisible }) {
+  const [typeLogin, setTypeLogin] = useState('');
+  const [isSelectType, setIsSelectType] = useState(false);
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className="wrapper_login">
+      <Modal
+        className="pop_login "
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        { isSelectType
+          ? <LoginForm typeLogin={typeLogin} />
+          : <TypeAccount setTypeLogin={setTypeLogin} setIsSelectType={setIsSelectType} />}
+      </Modal>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  setIsModalVisible: PropTypes.func.isRequired,
+  isModalVisible: PropTypes.bool.isRequired,
+};
+
+export default Login;
